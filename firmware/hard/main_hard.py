a0922cdef796bec12eca9bb83a65b3945aafc6f1
mPeskov23 <sorrowdelivery@gmail.com>
2025-06-26 20:54:17 +0200

    checkpoint

--- firmware/hard/main_hard.py
+++ firmware/hard/main_hard.py
@@ -3,7 +3,11 @@ from tenta_hard import Tentacle
 import time
 
 gripper = Tentacle()
+car = Car()
+
+
 
 for i in range(6):
     gripper.set_angle('arm', 90)
+    car.move_forward()
     time.sleep(0.5)
