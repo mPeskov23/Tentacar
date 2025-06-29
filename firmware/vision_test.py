19902ed54df36806a5ac9e4c210fa94265cbdabf
mPeskov23 <sorrowdelivery@gmail.com>
2025-06-29 18:21:27 +0200

    vision_test.py

---
+++ firmware/vision_test.py
@@ -0,0 +1,27 @@
+import cv2
+import time
+from vision import VisionModule
+from car import Car
+import utils_vision as utils
+
+vision = VisionModule()
+car = Car()
+
+for i in range(3):
+
+    frame = vision.picam2.capture_array()
+    frame = cv2.flip(frame, -1)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    tensor = vision.vision.TensorImage.create_from_array(frame_rgb)
+    detections = vision.detector.detect(tensor)
+
+    annotated = utils.visualize(frame, detections)
+
+    cv2.imshow("Detections", annotated)
+
+    print("Press any button to continue")
+    cv2.waitKey(0)
+    car.turn_left(3)
+
+cv2.destroyAllWindows()
