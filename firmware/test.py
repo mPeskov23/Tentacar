3df8b5490b00df154252988c0b2a37c0e40070d4
mPeskov23 <sorrowdelivery@gmail.com>
2025-06-29 17:39:33 +0200

    checkpoint

--- firmware/test.py
+++ firmware/test.py
@@ -1,12 +1,19 @@
 from car import Car
+from time import sleep
 
 car = Car()
 
 car.move_forward()
+sleep(1)
 car.speed = car.speed * 1.1
 car.move_forward()
+sleep(1)
 car.speed = car.speed * 1.1
-car.move_forward
+car.move_forward()
+sleep(1)
+car.speed = car.speed * 1.1
+car.move_forward()
+sleep(1)
 
 car.stop()
 car.cleanup()
