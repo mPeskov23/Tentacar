32b8ee72843089fb41f69a2d002ab13c2ea813ac
mPeskov23 <sorrowdelivery@gmail.com>
2025-06-30 16:12:54 +0200

    more gripper tests

--- firmware/tenta_test.py
+++ firmware/tenta_test.py
@@ -5,14 +5,21 @@ tenta = Tentacle(42)
 
 tenta.servos["base"].angle = 0
 sleep(1)
+tenta.servos["arm"].angle = 0
+sleep(1)
+tenta.servos["grip"].angle = 0
+sleep(1)
+
 tenta.servos["base"].angle = 180
+sleep(1)
+tenta.servos["arm"].angle = 180
+sleep(1)
+tenta.servos["grip"].angle = 180
 sleep(1)
 
 tenta.servos["base"].angle = 0
 sleep(1)
-
-tenta.servos["base"].angle = 180
+tenta.servos["arm"].angle = 0
 sleep(1)
-
-tenta.servos["base"].angle = 0
+tenta.servos["grip"].angle = 0
 sleep(1)
