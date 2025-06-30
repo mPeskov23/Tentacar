2367d1c652733d592cf84d7934cc047b4953df8d
mPeskov23 <sorrowdelivery@gmail.com>
2025-06-30 14:35:37 +0200

    checkpoint

--- firmware/utils_vision.py
+++ firmware/utils_vision.py
@@ -1,57 +1,31 @@
-# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Utility functions to display the pose detection results."""
-
 import cv2
 import numpy as np
-from tflite_support.task import processor
 
-_MARGIN = 10  # pixels
-_ROW_SIZE = 10  # pixels
+_MARGIN = 10
+_ROW_SIZE = 10
 _FONT_SIZE = 1
 _FONT_THICKNESS = 1
-_TEXT_COLOR = (0, 0, 255)  # red
+_TEXT_COLOR = (0, 0, 255)
 
+def visualize(image: np.ndarray, detections: list[dict]) -> np.ndarray:
+    """
+    Draw bounding boxes and class labels on the image.
 
-def visualize(
-    image: np.ndarray,
-    detection_result: processor.DetectionResult,
-) -> np.ndarray:
-  """Draws bounding boxes on the input image and return it.
+    Args:
+        image: The input RGB image (numpy array).
+        detections: A list of dicts with keys: 'xmin', 'ymin', 'xmax', 'ymax', 'class', 'score'
 
-  Args:
-    image: The input RGB image.
-    detection_result: The list of all "Detection" entities to be visualize.
+    Returns:
+        Annotated image.
+    """
+    for det in detections:
+        start_point = (det['xmin'], det['ymin'])
+        end_point = (det['xmax'], det['ymax'])
+        cv2.rectangle(image, start_point, end_point, _TEXT_COLOR, 2)
 
-  Returns:
-    Image with bounding boxes.
-  """
-  for detection in detection_result.detections:
-    # Draw bounding_box
-    bbox = detection.bounding_box
-    start_point = bbox.origin_x, bbox.origin_y
-    end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-    cv2.rectangle(image, start_point, end_point, _TEXT_COLOR, 3)
+        label = f"{det['class']} ({det['score']:.2f})"
+        text_location = (_MARGIN + det['xmin'], _MARGIN + _ROW_SIZE + det['ymin'])
+        cv2.putText(image, label, text_location, cv2.FONT_HERSHEY_PLAIN,
+                    _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
 
-    # Draw label and score
-    category = detection.categories[0]
-    category_name = category.category_name
-    probability = round(category.score, 2)
-    result_text = category_name + ' (' + str(probability) + ')'
-    text_location = (_MARGIN + bbox.origin_x,
-                     _MARGIN + _ROW_SIZE + bbox.origin_y)
-    cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
-
-  return image
+    return image
