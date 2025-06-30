0e4005a26af8eedfded139d233229fc93342a026
mPeskov23 <sorrowdelivery@gmail.com>
2025-06-30 14:58:12 +0200

    checkpoint

--- firmware/tenta_test.py
+++ firmware/tenta_test.py
@@ -3,10 +3,16 @@ from time import sleep
 
 tenta = Tentacle(42)
 
-tenta.servos["base"].angle = 90
+tenta.servos["base"].angle = 0
 sleep(1)
-tenta.servos["base"].angle = 140
+tenta.servos["base"].angle = 45
 sleep(1)
 
-tenta.servos["base"].angle = 10
+tenta.servos["base"].angle = 90
+sleep(1)
+
+tenta.servos["base"].angle = 135
+sleep(1)
+
+tenta.servos["base"].angle = 180
 sleep(1)
