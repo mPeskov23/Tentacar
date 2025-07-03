8ced5c8977f1a49fee41c9efe47d45c56e360b50
mPeskov23 <sorrowdelivery@gmail.com>
2025-07-03 18:22:22 +0200

    checkpoint

--- firmware/tenta_test.py
+++ firmware/tenta_test.py
@@ -3,11 +3,15 @@ from time import sleep
 
 tenta = Tentacle(42)
 
+print("0 degrees")
+
 tenta.servos["base"].angle = 0
 tenta.servos["arm"].angle = 0
 tenta.servos["grip"].angle = 0
 
 sleep(2)
+
+print("45 degrees")
 
 tenta.servos["base"].angle = 45
 tenta.servos["arm"].angle = 45
@@ -15,11 +19,15 @@ tenta.servos["grip"].angle = 45
 
 sleep(2)
 
+print("90 degrees")
+
 tenta.servos["base"].angle = 90
 tenta.servos["arm"].angle = 90
 tenta.servos["grip"].angle = 90
 
 sleep(2)
+
+print("135 degrees")
 
 tenta.servos["base"].angle = 135
 tenta.servos["arm"].angle = 135
@@ -27,8 +35,13 @@ tenta.servos["grip"].angle = 135
 
 sleep(2)
 
+print("180 degrees")
+
 tenta.servos["base"].angle = 180
 tenta.servos["arm"].angle = 180
 tenta.servos["grip"].angle = 180
 
 sleep(2)
+
+print("resetting position")
+tenta.reset_position()
