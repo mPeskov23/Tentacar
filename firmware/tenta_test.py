4babcae0c1824c18aea9a1e6571a545ab03af247
mPeskov23 <sorrowdelivery@gmail.com>
2025-07-03 18:35:30 +0200

    More interesting tenta_test.py

--- firmware/tenta_test.py
+++ firmware/tenta_test.py
@@ -9,39 +9,31 @@ tenta.servos["base"].angle = 0
 tenta.servos["arm"].angle = 0
 tenta.servos["grip"].angle = 0
 
-sleep(2)
+input("Press Enter to continue")
 
-print("45 degrees")
+print("smoothly moving to 45 degrees")
 
-tenta.servos["base"].angle = 45
-tenta.servos["arm"].angle = 45
-tenta.servos["grip"].angle = 45
+tenta.smooth_move("base", 0, 45)
+tenta.smooth_move("arm", 0, 45)
+tenta.smooth_move("grip", 0, 45)
 
-sleep(2)
+input("Press enter to continue")
 
-print("90 degrees")
+print("Quickly moving to 90 degrees")
 
 tenta.servos["base"].angle = 90
 tenta.servos["arm"].angle = 90
 tenta.servos["grip"].angle = 90
 
-sleep(2)
+input("Press enter to continue")
 
-print("135 degrees")
+print("Smooth moving to 135 degrees")
 
-tenta.servos["base"].angle = 135
-tenta.servos["arm"].angle = 135
-tenta.servos["grip"].angle = 135
+tenta.smooth_move("base", 90, 135)
+tenta.smooth_move("arm", 90, 135)
+tenta.smooth_move("grip", 90, 135)
 
-sleep(2)
-
-print("180 degrees")
-
-tenta.servos["base"].angle = 180
-tenta.servos["arm"].angle = 180
-tenta.servos["grip"].angle = 180
-
-sleep(2)
+input("Press Enter to finish")
 
 print("resetting position")
 tenta.reset_position()
