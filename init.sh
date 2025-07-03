4fda3e5e5d6f7223dff5bfa0af74da34e5d47f9f
mPeskov23 <sorrowdelivery@gmail.com>
2025-07-03 15:48:13 +0200

    init.sh update

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r requirements.txt
+venv/bin/pip install -r requirements.txt
 cd ./firmware
-python3 main.py
+venv/bin/python main.py
