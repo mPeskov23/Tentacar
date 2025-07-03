a78413e177143f7c0579f9a5ddb5feaee99a0f90
mPeskov23 <sorrowdelivery@gmail.com>
2025-07-03 18:19:38 +0200

    tenta-test.py new values

--- firmware/tenta_test.py
+++ firmware/tenta_test.py
@@ -4,22 +4,32 @@ from time import sleep
 tenta = Tentacle(42)
 
 tenta.servos["base"].angle = 0
-sleep(1)
 tenta.servos["arm"].angle = 0
-sleep(1)
 tenta.servos["grip"].angle = 0
-sleep(1)
+
+sleep(2)
+
+tenta.servos["base"].angle = 45
+tenta.servos["arm"].angle = 45
+tenta.servos["grip"].angle = 45
+
+sleep(2)
+
+tenta.servos["base"].angle = 90
+tenta.servos["arm"].angle = 90
+tenta.servos["grip"].angle = 90
+
+sleep(2)
+
+tenta.servos["base"].angle = 135
+tenta.servos["arm"].angle = 135
+tenta.servos["grip"].angle = 135
+
+sleep(2)
 
 tenta.servos["base"].angle = 180
 sleep(1)
 tenta.servos["arm"].angle = 180
 sleep(1)
 tenta.servos["grip"].angle = 180
-sleep(1)
-
-tenta.servos["base"].angle = 0
-sleep(1)
-tenta.servos["arm"].angle = 0
-sleep(1)
-tenta.servos["grip"].angle = 0
 sleep(1)
