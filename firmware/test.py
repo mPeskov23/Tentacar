c0f10b830fd468449ecd8315cedbc6b618d534c8
mPeskov23 <sorrowdelivery@gmail.com>
2025-07-04 16:04:15 +0200

    test.py

--- firmware/test.py
+++ firmware/test.py
@@ -3,31 +3,30 @@ from tenta import Tentacle
 from time import sleep
 
 car = Car()
-tenta = Tentacle(42)
 
+car.move_forward(1)
+sleep(1)
+car.turn_left(7)
 car.move_forward(2)
-sleep(0.2)
+
 
 car.turn_right(10)
-sleep(0.2)
-car.move_forward(2)
-sleep(0.2)
+car.move_forward(3)
 
-car.turn_left(10)
-sleep(0.2)
+car.stop()
 
-car.turn_right(10)
-sleep(0.2)
-car.move_forward(2)
-sleep(0.2)
+t = Tentacle(42)
 
-car.turn_left(10)
-sleep(0.2)
-car.move_forward(4)
-sleep(0.2)
-car.turn_left(5)
-sleep(0.2)
+t.servos["arm"].angle = 100
+sleep(0.5)
 
-car.move_forward(2)
+t.smooth_move("shoulder", 0, 165, 5)
+sleep(0.5)
+
+t.reset()
+sleep(0.5)
+
+t.servos["base"].angle = 90
 sleep(2)
-tenta.grab_test()
+
+t.reset()
