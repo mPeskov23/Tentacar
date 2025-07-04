4a03611775f0e2bdef196fe5310c6321c9dbc216
mPeskov23 <sorrowdelivery@gmail.com>
2025-07-04 15:44:20 +0200

    checkpoint

--- firmware/tenta_test.py
+++ firmware/tenta_test.py
@@ -1,47 +1,21 @@
 from tenta import Tentacle
 from time import sleep
 
-tenta = Tentacle(42)
+t = Tentacle(42)
 
-try:
-    while True:
-        print("0 degrees")
-        for angle in range(180, -1, -1):
-            try:
-                tenta.servos["base"].angle = angle
-                tenta.servos["shoulder"].angle = angle
-                tenta.servos["arm"].angle = angle
-                tenta.servos["grip"].angle = angle
-                sleep(0.2)
-            except KeyboardInterrupt:
-                break
+t.servos["base"].angle = 90
+sleep(2)
 
-        input("Press Enter to continue")
+t.servos["base"].angle = 0
+sleep(2)
 
-        print("Smoothly moving to 45 degrees")
-        tenta.smooth_move("base", 0, 45)
-        tenta.smooth_move("shoulder", 0, 45)
-        tenta.smooth_move("arm", 0, 45)
-        tenta.smooth_move("grip", 0, 45)
+t.servos["arm"].angle = 90
+sleep(2)
 
-        input("Press Enter to continue")
+t.servos["arm"].angle = 0
+sleep(2)
 
-        print("Smoothly moving to 90 degrees")
-        tenta.smooth_move("base", 45, 90)
-        tenta.smooth_move("shoulder", 45, 90)
-        tenta.smooth_move("arm", 45, 90)
-        tenta.smooth_move("grip", 45, 90)
+t.servos["shoulder"].angle = 90
+sleep(2)
 
-        input("Press Enter to continue")
-
-        print("Smooth moving to 135 degrees")
-        tenta.smooth_move("base", 90, 135)
-        tenta.smooth_move("shoulder", 90, 135)
-        tenta.smooth_move("arm", 90, 135)
-        tenta.smooth_move("grip", 90, 135)
-
-        break
-
-except KeyboardInterrupt:
-    print("Keyboard Interruption")
-
+t.servos["shoulder"].angle = 0
